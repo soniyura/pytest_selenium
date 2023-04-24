b5eda123ec1c194c453a05979caa54c5ebc07cf6
macbook <yorikyorik25@gmail.com>
2023-04-24 21:55:56 +0300

    Authorization

--- lesson.py
+++ lesson.py
@@ -5,8 +5,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 
 
-
-
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=options)
@@ -18,7 +16,57 @@ driver.maximize_window()
 #user_name = driver.find_element(By.NAME, "user-name") #NAME
 #user_name = driver.find_element(By.XPATH, '//*[@id="user-name"]') # full XPATH
 #user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]') #id XPATH
+#обираю поле логіна за допомогою XPATH
 user_name = driver.find_element(By.XPATH, "//input[@data-test='username']") #data-test XPATH
+#вставляємо значення в поле
 user_name.send_keys("standard_user")
+
+password = driver.find_element(By.CSS_SELECTOR, "#password") #CSS_SELECTOR
+password.send_keys("secret_sauce")
+
+#обираю кнопку та клікаю
+button_login = driver.find_element(By.XPATH, "//input[@value='Login']")
+button_login.click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
