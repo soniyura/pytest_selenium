aa97db4ad843612b4c22e5f84e4a7ab4327dfc4e
macbook <yorikyorik25@gmail.com>
2023-04-24 21:21:19 +0300

    add Webdriver Manager for Python

--- lesson.py
+++ lesson.py
@@ -1,12 +1,16 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 
-#driver = webdriver.Firefox(executable_path='/Users/macbook/AquaProjects/pytest_selenium/geckodriver')
+
+
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(executable_path='/Users/macbook/AquaProjects/pytest_selenium/chromedriver', chrome_options=options)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=options)
+
 
 driver.get('https://www.saucedemo.com/')
 driver.maximize_window()
