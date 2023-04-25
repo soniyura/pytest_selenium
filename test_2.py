05a5192ec7f4254fa1386b8555305813c75e45d2
macbook <yorikyorik25@gmail.com>
2023-04-25 21:44:56 +0300

    Refreshing the browser page.

--- test_2.py
+++ test_2.py
@@ -10,8 +10,8 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=options)
 
 base_url = 'https://www.saucedemo.com/'
-login_standard_user = "standard_user"
-password_all = "secret_sauc"
+login_standard_user = "standard_use"
+password_all = "secret_sauce"
 
 driver.get(base_url)
 driver.maximize_window()
@@ -26,18 +26,20 @@ button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
 button_login.click()
 print("клікнули на кнопку авторизації")
 
-#warring_test = driver.find_element(By.XPATH, "//h3[@data-test='error']") #знаходимо повідомлення про помилку
-#value_warring_test = warring_test.text      #читаємо повідомлення і зберігаємо в змінну
+warring_test = driver.find_element(By.XPATH, "//h3[@data-test='error']") #знаходимо повідомлення про помилку
+value_warring_test = warring_test.text      #читаємо повідомлення і зберігаємо в змінну
 #якщо значенн змінної відповідає тому що написано
-#assert value_warring_test == "Epic sadface: Username and password do not match any user in this service"
+assert value_warring_test == "Epic sadface: Username and password do not match any user in this service"
 #то виводимо прінт
-#print("good test")
+print("good test")
+
+driver.refresh() #оновлює сторінку
 
 
-warring_test2 = driver.find_element(By.XPATH, "//h3[@data-test='error']")
-value_warring_test2 = warring_test2.text
-assert value_warring_test2 == "Epic sadface: Username and password do not match any user in this service"
-print("неправельний пароль")
+#warring_test2 = driver.find_element(By.XPATH, "//h3[@data-test='error']")
+#alue_warring_test2 = warring_test2.text
+#assert value_warring_test2 == "Epic sadface: Username and password do not match any user in this service"
+#print("неправельний пароль")
 
 
 
