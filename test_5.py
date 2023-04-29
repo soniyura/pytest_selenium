323013f3ae63d965f322fcd69fc4512ce4aa4acb
macbook <yorikyorik25@gmail.com>
2023-04-29 23:00:39 +0300

    menu

--- test_5.py
+++ test_5.py
@@ -10,6 +10,7 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=options)
 
+"""ЗМІННІ"""
 base_url = 'https://www.saucedemo.com/'
 login_standard_user = "standard_user"
 password_all = "secret_sauce"
@@ -17,19 +18,26 @@ password_all = "secret_sauce"
 driver.get(base_url)
 driver.maximize_window()
 
+"""АВТОРИЗАЦІЯ"""
 user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
 user_name.send_keys(login_standard_user)
 print("ввели логін")
-time.sleep(3)
-user_name.clear()
+password = driver.find_element(By.XPATH, "//input[@id='password']")
+password.send_keys(password_all)
+print("ввели пароль")
+button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+button_login.click()
+print("клікнули на кнопку авторизації")
+
+"""БОКОВЕ МЕНЮ"""
+menu = driver.find_element(By.XPATH, "//button[@id='react-burger-menu-btn']")
+menu.click()
+time.sleep(2)
+print("клікнули на кнопку меню")
+link_about = driver.find_element(By.XPATH, "//a[@id='about_sidebar_link']")
+link_about.click()
 
 
-# password = driver.find_element(By.XPATH, "//input[@id='password']")
-# password.send_keys(password_all)
-# print("ввели пароль")
-# button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
-# button_login.click()
-# print("клікнули на кнопку авторизації")
 
 
 
